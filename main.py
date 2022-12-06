5f946598fd9f5889a8714502c39a327f58cfc79b
Shao <molly.shao@accenture.com>
2022-12-06 16:29:22 +0000

    updates

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, make_response
 import cocktail
+from waitress import serve
+import socket
+
 app = Flask(__name__)
 
 
@@ -10,33 +13,44 @@ def home():
 
 @app.route("/results", methods=['GET', 'POST'])
 def results():
-    print(str(request.form["ingredients"]))
+    # print(str(request.form["ingredients"]))
     ingredients = str(request.form["ingredients"])
-    print(ingredients)
+    # print(ingredients)
     search_results = cocktail.search(ingredients)
     return render_template('results.html', ingredients=ingredients, results=search_results)
 
 
 @app.route("/fav", methods=['GET', 'POST'])
 def fav():
-    print(request.form['drink'])
+    # print(request.form['drink'])
     df_result = cocktail.df1.loc[cocktail.df1['strDrink'] == request.form['drink']]
     df_result_ingre_list = df_result['clean_combined'].values
     ingredients = ' '.join(df_result_ingre_list)
-    print(ingredients)
+    # print(ingredients)
     search_results = cocktail.search(ingredients)
     return render_template('results.html', ingredients=ingredients, results=search_results)
 
 
 @app.route("/soundsgood", methods=['GET', 'POST'])
 def soundsgood():
-    print(request.args['drink'])
+    # print(request.args['drink'])
     df_result = cocktail.df1.loc[cocktail.df1['strDrink'] == request.args['drink']]
     df_result_ingre_list = df_result['clean_combined'].values
     ingredients = ' '.join(df_result_ingre_list)
-    print(ingredients)
+    # print(ingredients)
     search_results = cocktail.search(ingredients)
     return render_template('results.html', ingredients=ingredients, results=search_results)
 
 
-app.run()
+if __name__ == "__main__":
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    # local_url = "http://{}:{}".format(local_ip, PORT)
+    local_url = "http://{}".format(local_ip)
+    # print("http://{}:{}".format(local_ip, PORT))
+    print("http://{}".format(local_ip))
+    # -- prod using waitress --
+    serve(app, host="0.0.0.0", port=5000)   # prod run with waitress server
+    # serve(app, host="0.0.0.0", port=80, threads=6, _quiet=True)   # prod run with waitress server
+    # -- --
+    # app.run()
